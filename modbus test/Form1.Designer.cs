d99cf8aa41b638061bdd948c1bbb953f9c744bec
wexer <wexer@LAPTOP-KUTSQSVU>
2022-06-27 01:21:04 +0300

    Добавьте файлы проекта.

---
+++ modbus test/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace modbus_test
+{
+    partial class ReadHoldingRegistr
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.btnRHR = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(593, 12);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Port List:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(574, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRHR);
+            this.groupBox1.Controls.Add(this.textResult);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(492, 274);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RHR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RESULT";
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(6, 49);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(353, 132);
+            this.textResult.TabIndex = 1;
+            this.textResult.TextChanged += new System.EventHandler(this.textResult_TextChanged);
+            // 
+            // btnRHR
+            // 
+            this.btnRHR.Location = new System.Drawing.Point(78, 187);
+            this.btnRHR.Name = "btnRHR";
+            this.btnRHR.Size = new System.Drawing.Size(213, 34);
+            this.btnRHR.TabIndex = 2;
+            this.btnRHR.Text = "RHR";
+            this.btnRHR.UseVisualStyleBackColor = true;
+            this.btnRHR.Click += new System.EventHandler(this.btnRHR_Click);
+            // 
+            // ReadHoldingRegistr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 480);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ReadHoldingRegistr";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRHR;
+        private System.Windows.Forms.TextBox textResult;
+    }
+}
+
---
+++ modbus test/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+using Modbus.Device;
+using Modbus.Data;
+using Modbus.Utility;
+
+
+
+
+
+
+namespace modbus_test
+{
+    public partial class ReadHoldingRegistr : Form
+    {
+      
+
+        public ReadHoldingRegistr()
+        {
+            InitializeComponent();
+        }
+       
+       
+
+     
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //подлюкчает порт к устройству 
+           
+
+
+        }
+
+        private void textResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void btnRHR_Click(object sender, EventArgs e)
+               
+        {
+            SerialPort serialPort = null;
+            string portName = comboBox1.SelectedItem.ToString();
+            try
+            {
+                serialPort = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+                serialPort.Open();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                byte slaveAddress = 1;
+                ushort startAddress = 40001;
+                ushort numberOfPoints = 25;
+                IModbusMaster masterRTU = ModbusSerialMaster.CreateRtu(serialPort);
+                ushort[] result = masterRTU.ReadHoldingRegisters(slaveAddress, startAddress, numberOfPoints);
+
+                //display result 
+
+                textResult.Text = string.Empty;
+                foreach (ushort item in result)
+                {
+                    textResult.Text += string.Format("{0}/", item);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally 
+            {
+                serialPort.Close();
+            }
+        }
+
+        //com0com
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            var portList = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(portList);
+        }
+    }
+}
